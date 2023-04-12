4f54ebc5706d427061cae4ef727293d24209cd6b
Adnane Habib <adnanou.habibou@gmail.com>
2023-04-12 07:25:11 +0300

    Fixing appending in ascending order

--- RepositoriesVirtualKey/src/main/java/repositories/FilesList.java
+++ RepositoriesVirtualKey/src/main/java/repositories/FilesList.java
@@ -6,14 +6,19 @@ public class FilesList {
 	private FileNode head;
 	private int fileCounts = 0;
 	
-	public FileNode getHead() {
+	public FileNode getHead() 
+{
 		return head;
 	}
-	public int getListSize() {
+	public int getListSize() 
+{
 		return fileCounts;
 	}
 	
-	public void append(String fileName, int position) {
+	public void append(String fileName, int position) 
+{
+		// appending the elements in ascending order.
+		fileCounts++;
 		FileNode newNode = new FileNode(fileName, position);
 		if (head == null) { // Creates a new list if the list is empty
 			head = newNode;
@@ -21,33 +26,85 @@ public class FilesList {
 			return;
 		}
 		
-		FileNode current = head;	
-		
-		while(current.nextNode !=null && current.getFileName().compareTo(current.nextNode.getFileName())<0) {
-			current = current.nextNode;
-			System.out.println("I am in the while loop");
-					}
-		if (current.nextNode ==null) {
-			System.out.println("I am in the if ");
-			current.nextNode = newNode;
-		} else {
-			System.out.println("I am in the else");
-			current.nextNode = current;
-			current = newNode;
+
+		// checking if the element should be in the beginning.
+		if (head.getFileName().compareTo(newNode.getFileName())>0) {
+			
+			newNode.nextNode = head;
+			head = newNode;
+			System.out.println(newNode.getFileName()+" sucessfully added");
+			return;
 		}
+		
+		
+		FileNode current = head;
+		
+
+		while(current.nextNode !=null ) {
+			// traversing the elements and detecting the right spot where to insert the new element
+			
+			if (current.nextNode.getFileName().compareTo(newNode.getFileName())>0) {
+				
+				FileNode temp = current.nextNode;
+				
+				current.nextNode = newNode;
+				newNode.nextNode = temp;
+				System.out.println(newNode.getFileName()+" sucessfully added");
+				return;
+			}
+
+			
+			current = current.nextNode;
+					}
+		
+		current.nextNode = newNode;		
 		System.out.println(newNode.getFileName()+" sucessfully added");
 		}
 	
-	public void display() {
+	public void display() 
+	{
+		System.out.println("There are "+ getListSize() + " in the list.");
 		FileNode current = head;
 		
 		while(current!=null) {
 			System.out.println(current.getFileName()+" at "+ current.getFileLocation() );
 			current = current.nextNode;
 		}
-		System.out.println("null");
+		System.out.println();
 		
 	}
-	
 
+	public FileNode delete(String fileName) 
+	{
+		if (head==null) {
+			System.out.println("Linked list is empty, nothing to delete");
+			return null;
+		}
+		
+		if (head.getFileName()==fileName) {
+			head = head.nextNode;
+		}
+		
+		FileNode current = head;
+		FileNode previous = null;
+		
+		while(current!=null && current.getFileName()!=fileName) {
+			
+				previous = current;
+				current = current.nextNode;	
+
+		}
+				if 	(current!=null) {
+					previous.nextNode = current.nextNode;
+					System.out.println(fileName + " has been succesffully deleted.");
+					fileCounts--;
+					return current;
+				} else {
+					System.out.println("File not found, nothing to delete");
+					return null;
+				}		
+					
+		}
+	
+	
 }
--- RepositoriesVirtualKey/src/main/java/repositories/TestFileLists.java
+++ RepositoriesVirtualKey/src/main/java/repositories/TestFileLists.java
@@ -6,14 +6,25 @@ public class TestFileLists {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		FilesList list = new FilesList();
-		list.append("file2", 10);
-		list.append("file3", 20);
-		list.append("file1", 30);
-		list.append("file4", 40);
 		
-		//list.display();
+		FilesList list = new FilesList();
+		list.append("file8", 80);
+		list.append("file6", 60);
+		list.append("file7", 70);
+		list.append("file5", 50);
 
+		
+		list.display();
+		
+
+		list.append("file2", 20);
+		list.append("file3", 30);
+		list.append("file4", 40);
+		list.append("file1", 10);
+		
+		list.display();
+		list.delete("file3");
+		list.display();
 	}
 
 }
